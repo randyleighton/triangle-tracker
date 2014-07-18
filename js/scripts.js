3e74688c69da9a50bf589ddfa710c381c2ba42dd
Randy Leighton & Tanner Steward <student@epicodus.com>
2014-07-18 10:13:15 -0700

    removed files, updated code

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,12 @@
 
 $(document).ready(function() {
-
+document.forms['sides-input'].elements['side-one'].focus()
   $("form#sides-input").submit(function(event) {
 
     event.preventDefault();
     var inputtedSideOne = parseInt($("#side-one").val());
     var inputtedSideTwo = parseInt($("#side-two").val());
     var inputtedSideThree = parseInt($("#side-three").val());
-
-    var triangleTypes = {
-      equilateral: [],
-      isosceles: [],
-      scalene: []
-    };
-
     var triangleSides = {
       sideOne:  inputtedSideOne,
       sideTwo:  inputtedSideTwo,
@@ -26,16 +19,16 @@ $(document).ready(function() {
            if (this.sideOne === this.sideTwo && this.sideTwo === this.sideThree) {
               console.log("Triangle type: equilateral");
 
-              return "equilateral";
+              return $("#equil-triangle").append("<li>" + this.sideOne + ", " + this.sideTwo + ", " + this.sideThree + "</li>");
 
           } else if ( (this.sideOne === this.sideTwo) || (this.sideOne === this.sideThree) || (this.sideTwo === this.sideThree) ) {
               console.log("Triangle type: Isosceles");
 
-              return "isosceles";
+              return $("#isos-triangle").append("<li>" + this.sideOne + ", " + this.sideTwo + ", " + this.sideThree + "</li>");
           } else {
               console.log("Triangle type: Scalene");
 
-              return "scalene";
+              return $("#scal-triangle").append("<li>" + this.sideOne + ", " + this.sideTwo + ", " + this.sideThree + "</li>");
           } //end the loop for type of triangle
         } else {
           console.log("Test Failed: Invalid triangle");
@@ -46,5 +39,11 @@ $(document).ready(function() {
     }; //this ends var for object
 
     triangleSides.triangleCheck()
+
   });
+
+$("#reset-text").click(function() {
+        document.forms['sides-input'].elements['side-one'].focus();
+    });
+
 });
